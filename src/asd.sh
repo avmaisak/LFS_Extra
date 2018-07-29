2ddaf1aa8a89364e4a5f1e9ed221aa9f2688d4d1
Anton Maisak <am@rus-bit.com>
2018-07-29 12:46:18 +0300

    #

---
+++ src/asd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+source './utils.sh'
+source './cfg'
+
+if [ $# -eq 0 ]
+	then
+	msg_err "No arguments supplied"
+	exit 1
+fi
+
+case "$1" in
+ "help")
+    source './help'
+    ;;
+
+  "r")
+    msg_success "repository type: [$repo]"
+    msg_success "repository url:  [$repo_url]"
+    ;;
+
+  "v")
+     msg "asd version:  [$version]"
+    ;;
+
+	"a")
+     # msg "asd version:  [$version]"
+	  # curl -K myconfig.txt >> output.txt
+	  # D=$repo_url/$repo/$2
+	  # P=$(curl $D)
+	  # build()
+	;;
+
+  *)
+    echo "Такого аргумента нет. Смотрите информацию [#asd help]"
+    exit 1
+    ;;
+esac
