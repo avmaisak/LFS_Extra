fe5b216e6e67675c17cdf28c9f298044948d5b94
Anton Maisak <am@rus-bit.com>
2018-07-29 13:40:09 +0300

    #

--- src/asd.sh
+++ src/asd.sh
@@ -23,11 +23,34 @@ case "$1" in
     ;;
 
 	"a")
-     # msg "asd version:  [$version]"
-	  # curl -K myconfig.txt >> output.txt
-	  # D=$repo_url/$repo/$2
-	  # P=$(curl $D)
-	  # build()
+		if [ -z "$2" ]; then
+			echo "package not specified"
+			exit 1
+		fi
+
+		URL=$repo_url/$repo/$2
+		PKG_META_TEMP_DEST="$dest_temp/$2"
+
+		rm -rvf $PKG_META_TEMP_DEST
+
+		echo "installing package from url $URL to destination folder $PKG_META_TEMP_DEST"
+
+		curl --url $URL -K $2 >> $PKG_META_TEMP_DEST --silent
+		source "$PKG_META_TEMP_DEST"
+
+		# указан source
+		if [ "$source" ]; then
+			# имя файла
+			filename="${source##*/}"
+			# путь до загруженного архива
+			PKG_SRC="$dest_temp/$filename"
+			echo "package $PKG_SRC"
+			curl --url $URL -K $2 >> "$PKG_SRC" --silent
+			tar xvf $PKG_SRC
+			cd "$name-$version"
+		fi
+
+		build
 	;;
 
   *)
